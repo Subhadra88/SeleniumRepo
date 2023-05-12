ee3c137f8f853b9bf60bae260dc5f0b4928bf058
Subhadra88 <131670969+Subhadra88@users.noreply.github.com>
2023-05-12 12:38:24 +0530

    Add files via upload

---
+++ org/College/College.java
@@ -0,0 +1,13 @@
+package org.College;
+
+public class College {
+	public void collegeName() {
+		System.out.println("Pondicherry University");
+	}
+	public void collegeCode() {
+		System.out.println("College Code is 1234");
+	}
+	public void collegeRank() {
+		System.out.println("College Rank is 1");
+	}
+}
---
+++ org/System/Computer.java
@@ -0,0 +1,8 @@
+package org.System;
+
+public class Computer {
+	public void computerModel() {
+		System.out.println("My computer model is Lenovo 5");
+	}
+
+}
